1f62222b876bc9abd4b5d52d2c6da4552ac3f4d2
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2025-12-13 12:54:06 +0100

    Add gravity ui to the client package

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,3 +1,5 @@
+import '@gravity-ui/uikit/styles/fonts.css'
+import '@gravity-ui/uikit/styles/styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
@@ -5,12 +7,15 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 
 import { routes } from './routes'
+import { ThemeProvider } from '@gravity-ui/uikit'
 
 const router = createBrowserRouter(routes)
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
-  <Provider store={store}>
-    <RouterProvider router={router} />
-  </Provider>
+  <ThemeProvider>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </ThemeProvider>
 )
