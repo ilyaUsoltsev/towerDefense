e0879d5216234a943d2a04e9a715ebbb90f23910
Sprutoplut <rasl.galiullin55@gmail.com>
2025-12-14 19:04:43 +0500

    refresh form and login page

--- packages/client/src/components/FormLog/index.tsx
+++ packages/client/src/components/FormLog/index.tsx
@@ -1,40 +1,30 @@
 import React from 'react'
 import { Form } from 'react-final-form'
-import { DynamicField, Spec } from '@gravity-ui/dynamic-forms' // Импортируем Spec
 import styles from './FormLog.module.css'
-import { Button } from '@gravity-ui/uikit'
+import { Button, Link } from '@gravity-ui/uikit'
+
 interface FormLogProps {
   onSubmit: (values: Record<string, unknown>) => void | Promise<void>
-  spec: any // Теперь строго по типу библиотеки
-  config: any
   text: string
+  children: React.ReactNode
+  titleLink?: string
+  hrefLink?: string
   [key: string]: any
-}
-
-function isObjectSpec(spec: Spec): spec is Spec & {
-  type: 'object'
-  properties: Record<string, Spec>
-} {
-  return spec.type === 'object' && !!spec.properties
 }
 
 const FormLog: React.FC<FormLogProps> = ({
   onSubmit,
-  spec,
-  config,
   text,
+  children,
+  titleLink,
+  hrefLink,
   ...rest
 }) => {
-  const values = {
-    name: 'John Doe',
-    age: 30,
-    license: true,
-  }
   return (
-    <Form onSubmit={onSubmit}>
+    <Form onSubmit={onSubmit} {...rest}>
       {({ handleSubmit }) => (
-        <form onSubmit={handleSubmit} className={styles.form} {...rest}>
-          <DynamicField name="dynamicfield" spec={spec} config={config} />
+        <form onSubmit={handleSubmit} className={styles.form}>
+          {children}
           <Button
             width="max"
             view="action"
@@ -43,6 +33,7 @@ const FormLog: React.FC<FormLogProps> = ({
             className={styles.login__button}>
             {text}
           </Button>
+          <Link href={hrefLink || ''}> {titleLink} </Link>
         </form>
       )}
     </Form>
--- packages/client/src/pages/Login/index.tsx
+++ packages/client/src/pages/Login/index.tsx
@@ -1,57 +1,33 @@
 import { useSelector } from '../../store'
 import { fetchUserThunk, selectUser } from '../../slices/userSlice'
+import { TextInput } from '@gravity-ui/uikit'
 import { usePage } from '../../hooks/usePage'
 import { PageInitArgs } from '../../routes'
 import FormLog from '../../components/FormLog'
 import styles from './Login.module.css'
+import { Field } from 'react-final-form'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginPage = () => {
   const user = useSelector(selectUser)
+  const [formErrors, setFormErrors] = useState<Record<string, string> | null>(
+    null
+  )
   usePage({ initPage: initLoginPage })
-  const handleSubmit = (data: Record<string, string>) => {
+  const handleSubmit = (data: Record<string, unknown>) => {
+    const errors: Record<string, string> = {}
+
+    if (!data.login) errors.login = 'Поле обязательно для заполнения'
+    if (!data.password) errors.password = 'Поле обязательно для заполнения'
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors)
+      return // Не отправляем форму
+    }
+
+    setFormErrors(null)
     console.log('Валидация')
     console.log('Данные формы:', data)
-  }
-  const loginSpec = {
-    type: 'object',
-    properties: {
-      username: {
-        type: 'string',
-        viewSpec: {
-          type: 'base',
-          layout: 'row',
-          layoutTitle: 'Логин',
-        },
-      },
-      password: {
-        type: 'string',
-        viewSpec: {
-          type: 'base',
-          layout: 'row',
-          layoutTitle: 'Пароль',
-        },
-      },
-    },
-  }
-
-  // Конфигурация полей (config)
-  const loginConfig = {
-    fields: {
-      username: {
-        component: 'input',
-        props: {
-          type: 'text',
-          placeholder: 'Введите логин',
-        },
-      },
-      password: {
-        component: 'input',
-        props: {
-          type: 'password',
-          placeholder: 'Введите пароль',
-        },
-      },
-    },
   }
   return (
     <section className={styles.login__page}>
@@ -60,12 +36,35 @@ export const LoginPage = () => {
         alt="tower defence"
         className={styles.logo__big}
       />
-      <FormLog
-        onSubmit={handleSubmit}
-        spec={loginSpec}
-        config={loginConfig}
-        text="Войти"
-      />
+      <FormLog onSubmit={handleSubmit} text="Войти" titleLink="Нет аккаунта ?">
+        <Field name="login">
+          {({ input }) => (
+            <TextInput
+              {...input} // Передаём все пропсы от input (value, onChange и др.)
+              size="xl"
+              type="text"
+              placeholder="Логин"
+              validationState={formErrors?.login ? 'invalid' : undefined}
+              errorMessage={formErrors?.login || ''}
+              errorPlacement="outside"
+            />
+          )}
+        </Field>
+
+        <Field name="password">
+          {({ input }) => (
+            <TextInput
+              {...input}
+              size="xl"
+              type="password"
+              placeholder="Пароль"
+              validationState={formErrors?.password ? 'invalid' : undefined}
+              errorMessage={formErrors?.password || ''}
+              errorPlacement="outside"
+            />
+          )}
+        </Field>
+      </FormLog>
     </section>
   )
 }
