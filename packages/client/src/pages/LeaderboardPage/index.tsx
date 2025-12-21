1bd1fc46e78d910e2d216ace3e05bc00610b4008
Gleb Sysoikov <nemkocat@gmail.com>
2025-12-21 14:32:18 +0300

    Вынес PageHelmet в компонент

--- packages/client/src/pages/LeaderboardPage/index.tsx
+++ packages/client/src/pages/LeaderboardPage/index.tsx
@@ -1,7 +1,7 @@
-import { Helmet } from 'react-helmet';
 import { Table } from '@gravity-ui/uikit';
 import { useSelector } from '../../store';
 import { Header } from '../../components/Header';
+import { PageHelmet } from '../../components/PageHelmet';
 import { fetchUserThunk, selectUser } from '../../slices/userSlice';
 import { PageInitArgs } from '../../routes';
 import { usePage } from '../../hooks/usePage';
@@ -9,16 +9,16 @@ import styles from './LeaderboardPage.module.css';
 
 // Моковые данные для лидерборда
 const mockLeaderboard = [
-  { position: 1, username: 'Игрок1', score: 15000 },
-  { position: 2, username: 'Игрок2', score: 12000 },
-  { position: 3, username: 'Игрок3', score: 10000 },
-  { position: 4, username: 'Игрок4', score: 8500 },
-  { position: 5, username: 'Игрок5', score: 7200 },
-  { position: 6, username: 'Игрок6', score: 6000 },
-  { position: 7, username: 'Игрок7', score: 5000 },
-  { position: 8, username: 'Игрок8', score: 4500 },
-  { position: 9, username: 'Игрок9', score: 4000 },
-  { position: 10, username: 'Игрок10', score: 3500 },
+  { username: 'Игрок1', score: 15000 },
+  { username: 'Игрок2', score: 12000 },
+  { username: 'Игрок3', score: 10000 },
+  { username: 'Игрок4', score: 8500 },
+  { username: 'Игрок5', score: 7200 },
+  { username: 'Игрок6', score: 6000 },
+  { username: 'Игрок7', score: 5000 },
+  { username: 'Игрок8', score: 4500 },
+  { username: 'Игрок9', score: 4000 },
+  { username: 'Игрок10', score: 3500 },
 ];
 
 const getColumns = () => [
@@ -34,31 +34,23 @@ export const LeaderboardPage = () => {
 
   if (!user) {
     return (
-      <div className="App">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Лидерборд</title>
-          <meta name="description" content="Таблица лидеров" />
-        </Helmet>
+      <div>
+        <PageHelmet title="Лидерборд" description="Таблица лидеров" />
         <Header />
         <h3>Для просмотра лидерборда необходимо авторизоваться</h3>
       </div>
     );
   }
 
-  const data = mockLeaderboard.map(item => ({
-    position: item.position.toString(),
+  const data = mockLeaderboard.map((item, index) => ({
+    position: (index + 1).toString(),
     username: item.username,
     score: item.score.toLocaleString('ru-RU'),
   }));
 
   return (
-    <div className="App">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Лидерборд</title>
-        <meta name="description" content="Таблица лидеров" />
-      </Helmet>
+    <div>
+      <PageHelmet title="Лидерборд" description="Таблица лидеров" />
       <Header />
       <div className={styles.leaderboard__container}>
         <div className={styles.leaderboard__board}>
