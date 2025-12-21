82048957dd4ca4d39556d14edfaff530c6528969
Gleb Sysoikov <nemkocat@gmail.com>
2025-12-21 13:19:19 +0300

    Сделал компонент для страницы ошибки

--- packages/client/src/pages/Error400/index.tsx
+++ packages/client/src/pages/Error400/index.tsx
@@ -1,28 +1,18 @@
-import { Button } from '@gravity-ui/uikit';
-import { Link } from 'react-router-dom';
 import { usePage } from '../../hooks/usePage';
 import SectionLog from '../../components/SectionLog';
+import { ErrorContent } from '../../components/ErrorContent';
 import { PageInitArgs } from '../../routes';
-import styles from './Error400.module.css';
 
 export const Error400Page = () => {
   usePage({ initPage: initError400Page });
 
   return (
     <SectionLog>
-      <div className={styles.error__container}>
-        <h1 className={styles.error__code}>400</h1>
-        <h2 className={styles.error__title}>Неверный запрос</h2>
-        <p className={styles.error__description}>
-          К сожалению, ваш запрос содержит ошибку. Пожалуйста, проверьте
-          введенные данные и попробуйте снова.
-        </p>
-        <Link to="/">
-          <Button view="action" size="xl" width="max">
-            Вернуться на главную
-          </Button>
-        </Link>
-      </div>
+      <ErrorContent
+        code="400"
+        title="Неверный запрос"
+        description="К сожалению, ваш запрос содержит ошибку. Пожалуйста, проверьте введенные данные и попробуйте снова."
+      />
     </SectionLog>
   );
 };
