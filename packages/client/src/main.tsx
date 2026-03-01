ec92d1151820b40aab9a9140fb9ff5f57f85fadb
a-Aram-a <aramtashchan@gmail.com>
2026-03-01 23:27:04 +0300

    feat: :sparkles: added themes on frontend

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -8,16 +8,36 @@ import { store } from './store';
 import { routes } from './routes';
 import { ThemeProvider } from '@gravity-ui/uikit';
 import './index.css';
+import { FC, PropsWithChildren, useEffect } from 'react';
+import { useDispatch, useSelector } from './store';
+import { selectTheme, setTheme } from './slices/themeSlice';
+import { selectUser } from './slices/userSlice';
+import type { Theme } from './api/type';
+
+const ThemedApp: FC<PropsWithChildren> = ({ children }) => {
+  const theme = useSelector(selectTheme);
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    if (!user) {
+      const stored = localStorage.getItem('theme') as Theme | null;
+      if (stored) dispatch(setTheme(stored));
+    }
+  }, []);
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
-  <ThemeProvider>
-    <Provider store={store}>
+  <Provider store={store}>
+    <ThemedApp>
       <RouterProvider router={router} />
-    </Provider>
-  </ThemeProvider>
+    </ThemedApp>
+  </Provider>
 );
 
 startServiceWorker();
